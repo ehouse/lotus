2f78f9c571a539a839960eba1b5ebb6992f327b6
Evelyn House <eve@ehouse.io>
2021-12-03 23:12:46 -0500

    Updating type safety of [id].tsx

--- README.md
+++ README.md
@@ -6,17 +6,16 @@ First, run the development server:
 
 ```bash
 npm run dev
-# or
-yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+Files placed in blog/_posts will be compiled and deployed as blog posts. 
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+Static files placed in blog/_static will be copied over at build time as is. 
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+The default destination file is ./dest
+
 
 ## Learn More
 
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,6 +1,8 @@
 
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Error from 'next/error';
+
 import { useRouter } from 'next/router';
 import BlogContent from '../../dest/content.json';
 import BlogSideCar from '../../dest/sidecar.json';
@@ -22,11 +24,22 @@ const Post: NextPage = () => {
   const { id } = router.query;
 
   if (typeof id === 'string') {
+
+    // TODO Resolve this awful type casting 
+    if (BlogSideCar[id as PostTitle] === undefined) {
+      return <Error statusCode={404} />;
+    }
+
+    const postTitleQuery = id as PostTitle;
+
+    const postTitle = BlogSideCar[postTitleQuery].Title;
+    const postContent = BlogContent[postTitleQuery];
+
     return <>
       <Head>
-        <title>{BlogSideCar[id as PostTitle].Title}</title>
+        <title>{postTitle}</title>
       </Head>
-      <div dangerouslySetInnerHTML={{ __html: BlogContent[id as PostTitle] }}></div>
+      <div dangerouslySetInnerHTML={{ __html: postContent }}></div>
     </>;
   }
 
